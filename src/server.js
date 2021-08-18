122f1bdc03b7aa9f42a80e4191c2f763b15b0918
Arturo <arturor22010@hotmail.com>
2021-08-18 18:56:12 -0500

    5-vistas

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
 const path = require('path');
 
 // Initializations
@@ -6,7 +7,15 @@ const app = express();
 
 // Settings
 app.set('port', process.env.PORT || 4000);
-app.set('views', path.join(__dirname + 'views'));
+app.set('views', path.join(__dirname + '/views'));
+app.engine('.hbs', exphbs({
+    defaultLayout: 'main',
+    layoutsDir: path.join(app.get('views'), '/layouts'),
+    partialsDir: path.join(app.get('views'), '/partials'),
+    extname: '.hbs'
+}));
+
+app.set('view engine', '.hbs');
 
 //Middlewares
 app.use(express.urlencoded({extended: false}));
@@ -15,7 +24,7 @@ app.use(express.urlencoded({extended: false}));
 
 // Routes
 app.get('/', (req, res) => {
-    res.send('Holaaaa');
+    res.render('index');
 });
 
 // Static fles
